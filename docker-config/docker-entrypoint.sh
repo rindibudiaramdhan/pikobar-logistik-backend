f779f9e7fb5842ef10ba9cd77888d32eecf17bec
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-05-25 05:16:54 +0700

    add php artisan migrate

--- docker-config/docker-entrypoint.sh
+++ docker-config/docker-entrypoint.sh
@@ -2,6 +2,23 @@
 app=${DOCKER_APP:-app}
 
 if [ "$app" = "app" ]; then
+    #!/bin/bash
+    printf "Checking database connection...\n\n"
+    mysql_ready() {
+        /usr/bin/mysqladmin ping --host=$DB_HOST --user=$DB_USERNAME --password=$DB_PASSWORD > /dev/null 2>&1
+    }
+
+    while !(mysql_ready)
+    do
+        sleep 3
+        echo "Waiting for database connection ..."
+    done
+
+    php composer.phar dump-autoload
+    php artisan config:clear
+    php artisan cache:clear
+    php artisan route:clear
+    php artisan migrate --no-interaction -vvv --force
 
     echo "Running the app..."
     /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
